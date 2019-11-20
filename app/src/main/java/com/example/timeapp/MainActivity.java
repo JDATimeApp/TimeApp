96358e59bc35b48a102874963474cc06f9fa9ea2
spozap <sergi.poza@iesjoandaustria.org>
2019-11-20 19:22:43 +0100

    Modificado manifest y creado MainActivity

---
+++ app/src/main/java/com/example/timeapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.timeapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = new loginFragment();
+
+        fm.beginTransaction().replace(R.id.fragment_container,fragment,"Login").commit();
+    }
+}
