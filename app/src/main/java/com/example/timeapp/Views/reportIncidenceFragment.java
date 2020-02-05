068751e666b9e0ccd69ac273484e09385ad7da8a
spozap <sergi.poza@iesjoandaustria.org>
2020-02-05 18:38:04 +0100

    added recyclerView for entries

--- app/src/main/java/com/example/timeapp/Views/reportIncidenceFragment.java
+++ app/src/main/java/com/example/timeapp/Views/reportIncidenceFragment.java
@@ -2,12 +2,14 @@ package com.example.timeapp.Views;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Scroller;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
