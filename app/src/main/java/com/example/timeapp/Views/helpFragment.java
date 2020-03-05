327b0dad32c7ce63690eb9a454c332e2a28ce1e7
spozap <sergi.poza@iesjoandaustria.org>
2020-03-05 19:18:11 +0100

    helpFragment.java : example videoView

--- app/src/main/java/com/example/timeapp/Views/helpFragment.java
+++ app/src/main/java/com/example/timeapp/Views/helpFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,8 @@ import com.example.timeapp.ViewModels.HelpViewModel;
 public class helpFragment extends Fragment {
 
     private HelpViewModel helpViewModel;
+    private VideoView video;
+    private MediaController mediaController;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,6 +28,18 @@ public class helpFragment extends Fragment {
                 ViewModelProviders.of(this).get(HelpViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_help, container, false);
+
+        video = root.findViewById(R.id.helpVideo);
+
+        video.setVideoPath("android.resource://"+
+                getContext().getPackageName()+"/"+R.raw.video1);
+
+        mediaController = new MediaController(getContext());
+
+        video.setMediaController(mediaController);
+
+        mediaController.setAnchorView(video);
+        mediaController.setMediaPlayer(video);
 
         return root;
     }
