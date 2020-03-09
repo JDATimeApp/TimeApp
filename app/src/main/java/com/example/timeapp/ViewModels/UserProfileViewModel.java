08c39fb16678ba9797bd5a446b03bc0afade83a2
ismael.romero <ismaelromero1999@gmail.com>
2020-03-09 20:18:42 +0100

    UserProfileFragment.java:created listen, override onActivityResult UserProfileViewModel.java: chargeImageGalery method

--- app/src/main/java/com/example/timeapp/ViewModels/UserProfileViewModel.java
+++ app/src/main/java/com/example/timeapp/ViewModels/UserProfileViewModel.java
@@ -1,10 +1,16 @@
 package com.example.timeapp.ViewModels;
 
 
+import android.content.Intent;
+import android.provider.MediaStore;
+
 import androidx.lifecycle.ViewModel;
 
 public class UserProfileViewModel extends ViewModel {
-
+    public static Intent chargeImageGalery(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        return intent;
+    }
 
 
 }
